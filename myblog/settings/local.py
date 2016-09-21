81aa68ae128646a49038c2f61d7275991f2c603b
userimack <mahendra.k12@gmail.com>
2016-09-21 13:29:38 +0530

    Arranged settings file for local and production

---
+++ myblog/settings/local.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from myblog.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ myblog/settings/production.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from myblog.settings.base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
